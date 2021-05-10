2febb901cb82d8ddbeb00acf67bcc63ea5ff9b2c
solmir2000 <jean.romagnani@gmail.com>
2021-05-10 17:41:00 +0200

    dialog ui fini

--- lautrec game/Assets/Scripts/Talker.cs
+++ lautrec game/Assets/Scripts/Talker.cs
@@ -7,13 +7,21 @@ using UnityEngine.UI;
 public class Talker : MonoBehaviour
 {
 
-    public enum TalkerState {Talker, Neutral, Listen};
+    public enum TalkerState { Talker, Neutral, Listen };
     public TalkerState currentState = TalkerState.Neutral;
 
-    Image image;
+    public bool baseSpriteIsRight = true;
+
+    public Image image;
+    public RectTransform imageTr;
     RectTransform tr;
 
     public bool isRight = false;
+    public float lowerBound = 210;
+    public float LateralPos = 210;
+
+    float yPos;
+    float xPos;
 
     public struct StateValue
     {
@@ -23,20 +31,18 @@ public class Talker : MonoBehaviour
 
         public StateValue(Vector2 i, Vector2 j, Color c)
         {
-                this.pos = i;
-                this.size = j;
-                this.color = c;
+            this.pos = i;
+            this.size = j;
+            this.color = c;
         }
     }
 
-
-    StateValue listenValue = new StateValue(new Vector2(260, 360),new Vector2(300, 300), new Color(76, 76, 76, 255));
-    StateValue neutralValue = new StateValue(new Vector2(260, 360),new Vector2(325, 325), new Color(255, 255, 255, 255));
-    StateValue talkerValue = new StateValue(new Vector2(260, 360),new Vector2(350, 350),new Color(255, 255, 255, 255));
+    StateValue listenValue = new StateValue(new Vector2(0, 0), new Vector2(300, 300), new Color(76, 76, 76, 255));
+    StateValue neutralValue = new StateValue(new Vector2(0, 0), new Vector2(325, 325), new Color(255, 255, 255, 255));
+    StateValue talkerValue = new StateValue(new Vector2(0, 0), new Vector2(350, 350), new Color(255, 255, 255, 255));
 
     void start()
     {
-        image = GetComponent<Image>();
         tr = GetComponent<RectTransform>();
     }
 
@@ -45,33 +51,36 @@ public class Talker : MonoBehaviour
         image.sprite = sp;
     }
 
+    void CalcPos()
+    {
+        yPos = lowerBound + imageTr.sizeDelta.y / 2;
+        if (isRight == true)
+            xPos = Screen.currentResolution.width - LateralPos;
+        else
+            xPos = LateralPos;
+    }
+
+    void DirectionSprite()
+    {
+        float rotaY = 0;
+        if (baseSpriteIsRight == false)
+            rotaY = 180;
+        if (isRight == true)
+            rotaY += 180;
+        imageTr.rotation = Quaternion.Euler(0, rotaY, 0);
+    }
+
     void SetStateValue(StateValue val)
     {
+        DirectionSprite();
+        imageTr.sizeDelta = val.size;
+        CalcPos();
+        val.pos = new Vector2(xPos, yPos);
         tr.position = val.pos;
-        tr.sizeDelta = val.size;
         image.color = new Color32((byte)val.color.r, (byte)val.color.g, (byte)val.color.b, (byte)val.color.a);
     }
 
-
- // StateValue lv()
-    // {
-    //      return (new StateValue(new Vector2(260, 360),new Vector2(300, 300), new Color(76, 76, 76, 255));
-    // }
-    
- // StateValue nv()
-    // {
-    //     return (new StateValue(new Vector2(260, 360),new Vector2(325, 325), new Color(255, 255, 255, 255));
-    // }
-
-    // StateValue tv()
-    // {
-    //     return (new StateValue(new Vector2(260, 360),new Vector2(350, 350),new Color(255, 255, 255, 255));
-    // }
-
-   
-   
-
-    private void OnValidate() 
+    private void OnValidate()
     {
         if (!tr)
             tr = GetComponent<RectTransform>();
@@ -82,45 +91,25 @@ public class Talker : MonoBehaviour
 
     void ChangeState()
     {
-        switch(currentState)
+        switch (currentState)
         {
             case TalkerState.Talker:
-            {
-                SetStateValue(talkerValue);
-                break;
-            }
+                {
+                    SetStateValue(talkerValue);
+                    break;
+                }
             case TalkerState.Neutral:
-            {
-                SetStateValue(neutralValue);
-                break;
-            }
+                {
+                    SetStateValue(neutralValue);
+                    break;
+                }
             case TalkerState.Listen:
-            {
-                SetStateValue(listenValue);
-                break;
-            }
+                {
+                    SetStateValue(listenValue);
+                    break;
+                }
             default:
                 break;
         }
-        // switch(currentState)
-        // {
-        //     case TalkerState.Talker:
-        //     {
-        //         SetStateValue(tv());
-        //         break;
-        //     }
-        //     case TalkerState.Neutral:
-        //     {
-        //         SetStateValue(nv());
-        //         break;
-        //     }
-        //     case TalkerState.Listen:
-        //     {
-        //         SetStateValue(lv());
-        //         break;
-        //     }
-        //     default:
-        //         break;
-        // }
     }
 }
